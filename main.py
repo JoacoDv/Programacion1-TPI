041d4eba32ced11240c170479c8a9936358c07da
Joaco <joacodv2003@gmail.com>
2025-09-03 14:44:09 -0300

    guia

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+# 1ro Hay que pedir al usuario el nombre del archivo con el cual vamos a trabajar
+
+# 2do leer ese archivo
+
+# 3ro guardar los datos en nuestro programa
+
+# 4to vamos a imprimir por pantalla las opciones de 1) Filtrar  paises, 2) ordenar paises, 3) Mostrar estadisticas, 4) Buscar un pais. 5) Salir del programa.
+
+# 5to volver a mostrar hasta que decida salir
